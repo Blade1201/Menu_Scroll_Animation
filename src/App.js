fcfe711b8cbf142b12327175da91d1586273bb8e
Aaron <mol.aron@hotmail.com>
2023-04-06 18:37:16 +0200

    Added to portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./styles/main.css";
+import "./styles/menu.css";
 
-function App() {
+const App = () => {
+
+    const [Pos, setPos] = useState(8);
+
+    useEffect(() => {
+        function handleScroll(event) {
+            const scrollPos = window.scrollY;
+            const newPos = Math.max(13, 90 - scrollPos / 10);
+            setPos(newPos);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="container">
+        <ul className="nav">
+          <li onMouseOver={() => setPos(13)}><a href="#">Menü</a></li>
+          <li onMouseOver={() => setPos(83)}><a className="hsubs" href="#">Menü</a></li>
+
+          <li onMouseOver={() => setPos(155)}>
+                 <a href="#">Menü1</a>
+             <ul className="subs">
+                 <li><a href="">Almenü</a></li>
+                 <li><a href="">Almenü</a></li>
+                 <li><a href="">Almenü</a></li>
+                 <li><a href="">Almenü</a></li>
+                 <li><a href="">Almenü</a></li>
+                 <li><a href="">Almenü</a></li>
+             </ul>
+          </li>
+
+          <li onMouseOver={() => setPos(230)}><a href="#">Menü</a></li>
+          <li onMouseOver={() => setPos(300)}><a href="#">Menü</a></li>
+          <li onMouseOver={() => setPos(370)}><a href="#">Menü</a></li>
+          <li onMouseOver={() => setPos(440)}><a href="#">Menü</a></li>
+          <li onMouseOver={() => setPos(510)}><a href="#">Menü</a></li>
+            <div className="slider" style={{ left: Pos }}></div>
+        </ul>
+      </div>
+
     </div>
   );
 }
